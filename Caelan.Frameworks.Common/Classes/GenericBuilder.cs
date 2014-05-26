888bb0177d7669b9ded2ff7d37a4cf6084392e8a
Matteo Bruni <matteo.bruni@me.com>
2014-05-26 20:26:57 +0200

    Changed GenericBuilder creation for generic builder, it uses an existing builder if found.

--- Caelan.Frameworks.Common/Classes/GenericBuilder.cs
+++ Caelan.Frameworks.Common/Classes/GenericBuilder.cs
@@ -1,18 +1,25 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
 
 namespace Caelan.Frameworks.Common.Classes
 {
-	public static class GenericBuilder
-	{
-		public static BaseBuilder<TSource, TDestination> Create<TSource, TDestination>()
-			where TDestination : class, new()
-			where TSource : class, new()
-		{
-			var builder = new BaseBuilder<TSource, TDestination>();
+    public static class GenericBuilder
+    {
+        public static BaseBuilder<TSource, TDestination> Create<TSource, TDestination>()
+            where TDestination : class, new()
+            where TSource : class, new()
+        {
+            var builder = new BaseBuilder<TSource, TDestination>();
 
-			if (Mapper.FindTypeMapFor<TSource, TDestination>() == null) Mapper.AddProfile(builder);
+            var customBuilder = Assembly.GetCallingAssembly().GetTypes().Where(t => t.BaseType == builder.GetType()).Select(Activator.CreateInstance).SingleOrDefault();
 
-			return builder;
-		}
-	}
+            if (customBuilder != null) return customBuilder as BaseBuilder<TSource, TDestination>;
+
+            if (Mapper.FindTypeMapFor<TSource, TDestination>() == null) Mapper.AddProfile(builder);
+
+            return builder;
+        }
+    }
 }

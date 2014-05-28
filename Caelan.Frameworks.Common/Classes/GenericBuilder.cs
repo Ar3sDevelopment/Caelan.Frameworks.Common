3dca2c38fb20148738e4301ae63b6c25898e9812
Matteo Bruni <matteo.bruni@me.com>
2014-05-28 19:49:38 +0200

    Added CreateGenericBuilder for common creation of custom builders

--- Caelan.Frameworks.Common/Classes/GenericBuilder.cs
+++ Caelan.Frameworks.Common/Classes/GenericBuilder.cs
@@ -11,11 +11,23 @@ namespace Caelan.Frameworks.Common.Classes
             where TDestination : class, new()
             where TSource : class, new()
         {
-            var builder = new BaseBuilder<TSource, TDestination>();
+            return CreateGenericBuilder<BaseBuilder<TSource, TDestination>, TSource, TDestination>();
+        }
 
-            var customBuilder = Assembly.GetExecutingAssembly().GetReferencedAssemblies().OrderBy(t => t.Name).Select(Assembly.Load).SelectMany(assembly => assembly.GetTypes().Where(t => t.BaseType == builder.GetType())).SingleOrDefault();
+        public static TBuilder CreateGenericBuilder<TBuilder, TSource, TDestination>()
+            where TBuilder : BaseBuilder<TSource, TDestination>
+            where TSource : class, new()
+            where TDestination : class, new()
+        {
+            var builder = Activator.CreateInstance<TBuilder>();
 
-            if (customBuilder != null) return Activator.CreateInstance(customBuilder) as BaseBuilder<TSource, TDestination>;
+            var customBuilder = Assembly.GetAssembly(typeof(TDestination)).GetTypes().SingleOrDefault(t => t.BaseType == builder.GetType()) ?? (Assembly.GetAssembly(typeof(TDestination))).GetReferencedAssemblies().OrderBy(t => t.Name).Select(Assembly.Load).SelectMany(assembly => assembly.GetTypes().Where(t => t.BaseType == builder.GetType())).SingleOrDefault();
+
+            if (customBuilder != null) return Activator.CreateInstance(customBuilder) as TBuilder;
+
+            customBuilder = (Assembly.GetEntryAssembly() ?? Assembly.GetCallingAssembly()).GetTypes().SingleOrDefault(t => t.BaseType == builder.GetType()) ?? (Assembly.GetEntryAssembly() ?? Assembly.GetCallingAssembly()).GetReferencedAssemblies().OrderBy(t => t.Name).Select(Assembly.Load).SelectMany(assembly => assembly.GetTypes().Where(t => t.BaseType == builder.GetType())).SingleOrDefault();
+
+            if (customBuilder != null) return Activator.CreateInstance(customBuilder) as TBuilder;
 
             if (Mapper.FindTypeMapFor<TSource, TDestination>() == null) Mapper.AddProfile(builder);
 

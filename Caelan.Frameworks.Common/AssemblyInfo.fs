a33087000df8cc63822e989739ca2f9cec80b135
Matteo Bruni <matteo.bruni@me.com>
2014-06-24 18:18:18 +0200

    Added AssemblyInfo

---
+++ Caelan.Frameworks.Common/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿namespace Caelan.Frameworks.Common
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Caelan.Frameworks.Common")>]
+[<assembly: AssemblyDescription("Common Framework")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Caelan")>]
+[<assembly: AssemblyProduct("Caelan.Frameworks.Common")>]
+[<assembly: AssemblyCopyright("Caelan © 2014")>]
+[<assembly: AssemblyTrademark("")>]
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("f339802d-371c-4ff1-929b-5c1f4a37381a")>]
+
+[<assembly: AssemblyVersion("2.0.1.*")>]
+
+
+()

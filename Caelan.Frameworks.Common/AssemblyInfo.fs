eea85b51cb6c61f57a93133619d285a10695b06c
Matteo Bruni <matteo.bruni@me.com>
2014-07-11 15:07:14 +0200

    Updated version

--- Caelan.Frameworks.Common/AssemblyInfo.fs
+++ Caelan.Frameworks.Common/AssemblyInfo.fs
@@ -13,5 +13,5 @@ open System.Runtime.InteropServices
 [<AssemblyTrademark("")>]
 [<ComVisible(false)>]
 [<Guid("f339802d-371c-4ff1-929b-5c1f4a37381a")>]
-[<AssemblyVersion("2.1.1.*")>]
+[<AssemblyVersion("2.2.0.*")>]
 ()

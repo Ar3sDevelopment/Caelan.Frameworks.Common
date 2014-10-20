f8154b7a6537f00830eab96c2f147a6744fdc64a
Matteo Bruni <Ar3sHack@gmail.com>
2014-10-20 15:41:06 +0200

    Fix Generic IMapper

--- Caelan.Frameworks.Common/IMapper.fs
+++ Caelan.Frameworks.Common/IMapper.fs
@@ -1,5 +1,5 @@
 ﻿namespace Caelan.Frameworks.Common.Interfaces
 
-type IMapper<'TSource, 'TDestination> = 
+type IMapper<'TSource, 'TDestination when 'TSource : equality and 'TDestination : equality and 'TSource : null and 'TDestination : null> = 
     abstract Map : 'TSource -> 'TDestionation
     abstract Map : 'TSource * 'TDestionation ref -> unit

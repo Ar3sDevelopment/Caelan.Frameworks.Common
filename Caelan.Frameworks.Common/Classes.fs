464f9cc0d22269c20b4f6b7791fb3a143c6ebcba
Matteo Bruni <Ar3sHack@gmail.com>
2014-10-20 19:04:53 +0200

    Fixed Mapper and Builder types

--- Caelan.Frameworks.Common/Classes.fs
+++ Caelan.Frameworks.Common/Classes.fs
@@ -3,7 +3,7 @@
 open Caelan.Frameworks.Common.Interfaces
 
 [<AbstractClass; Sealed>]
-type Builder<'TSource, 'TDestination when 'TSource : equality and 'TDestination : equality and 'TSource : null and 'TDestination : null>(mapper : IMapper<'TSource, 'TDestination>) = 
+type Builder<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct>(mapper : IMapper<'TSource, 'TDestination>) = 
     member __.Build(source : 'TSource) = mapper.Map(source)
     member this.BuildList(sourceList) = sourceList |> Seq.map (fun source -> this.Build(source))
     member __.Build(source : 'TSource, destination : 'TDestination ref) = mapper.Map(source, destination)
--- Caelan.Frameworks.Common/IMapper.fs
+++ Caelan.Frameworks.Common/IMapper.fs
@@ -1,5 +1,5 @@
 ﻿namespace Caelan.Frameworks.Common.Interfaces
 
-type IMapper<'TSource, 'TDestination when 'TSource : equality and 'TDestination : equality and 'TSource : null and 'TDestination : null> = 
+type IMapper<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct> = 
     abstract Map : 'TSource -> 'TDestionation
     abstract Map : 'TSource * 'TDestionation ref -> unit

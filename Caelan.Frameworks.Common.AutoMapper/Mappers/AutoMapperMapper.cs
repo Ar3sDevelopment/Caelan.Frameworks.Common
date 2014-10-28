8b175e4423135882e2380b520b7b2bae51bdda0d
Matteo Bruni <Ar3sHack@gmail.com>
2014-10-28 14:31:00 +0100

    Added AutoMapper project and DefaultMapper abstract class

---
+++ Caelan.Frameworks.Common.AutoMapper/Mappers/AutoMapperMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Caelan.Frameworks.Common.Interfaces;
+using Microsoft.FSharp.Core;
+
+namespace Caelan.Frameworks.Common.AutoMapper.Mappers
+{
+	public class AutoMapperMapper<TSource, TDestination> : Profile, IMapper<TSource, TDestination>
+		where TSource : class
+		where TDestination : class
+	{
+		public TDestination Map(TSource source)
+		{
+			TDestination dest = null;
+			var destRef = new FSharpRef<TDestination>(dest);
+
+			Map(source, destRef);
+
+			return destRef.Value;
+		}
+
+		public void Map(TSource source, FSharpRef<TDestination> destination)
+		{
+			Mapper.Map(source, destination.Value);
+		}
+	}
+}
---
+++ Caelan.Frameworks.Common/DefaultMapper.fs
@@ -0,0 +1,17 @@
+﻿namespace Caelan.Frameworks.Common.Classes
+
+open Caelan.Frameworks.Common.Interfaces
+
+[<AbstractClass>]
+type DefaultMapper<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct>() = 
+    
+    interface IMapper<'TSource, 'TDestination> with
+        member this.Map(source : 'TSource, destination : 'TDestination ref) = this.Map(source, destination)
+        member this.Map(source) = 
+            let dest : 'TDestination = null
+            let refDest = ref dest
+            (this :> IMapper<'TSource, 'TDestination>).Map(source, refDest)
+            !refDest
+    
+    abstract Map : 'TSource * 'TDestination ref -> unit
+    member this.Map(source) = (this :> IMapper<'TSource, 'TDestination>).Map(source)

f1e1d38231610baece42e11ce32e8c182ff3f837
Matteo Bruni <ar3shack@gmail.com>
2014-12-17 00:08:40 +0100

    Removed MapType, bad idea

--- Caelan.Frameworks.Common/Classes/DefaultMapper.fs
+++ Caelan.Frameworks.Common/Classes/DefaultMapper.fs
@@ -2,28 +2,21 @@
 
 open System
 open Caelan.Frameworks.Common.Interfaces
-open Caelan.Frameworks.Common.Enums
 
 [<AbstractClass>]
 type DefaultMapper<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct>() = 
     
     interface IMapper<'TSource, 'TDestination> with
-        member this.Map(source : 'TSource, destination : 'TDestination byref, mapType : MapType) = this.Map(source, ref destination, mapType)
         member this.Map(source : 'TSource, destination : 'TDestination byref) = this.Map(source, ref destination)
-        member this.Map(source) = this.Map(source, MapType.NewObject)
-        member this.Map(source : 'TSource, mapType : MapType) = 
+        member this.Map(source : 'TSource) = 
             match source with
             | null -> Unchecked.defaultof<'TDestination>
             | _ -> 
                 let destination = Activator.CreateInstance<'TDestination>()
-                (this :> IMapper<'TSource, 'TDestination>).Map(source, ref destination, mapType)
+                (this :> IMapper<'TSource, 'TDestination>).Map(source, ref destination)
                 destination
     
     abstract Map : source:'TSource * destination:'TDestination byref -> unit
     abstract Map : source:'TSource -> 'TDestination
-    abstract Map : source:'TSource * mapType: MapType -> 'TDestination
-    abstract Map : source:'TSource * destination:'TDestination byref * mapType:MapType -> unit
 
-    override this.Map(source : 'TSource, destination : 'TDestination byref) = (this :> IMapper<'TSource, 'TDestination>).Map(source, ref destination, MapType.EditObject)
     override this.Map(source) = (this :> IMapper<'TSource, 'TDestination>).Map(source)
-    override this.Map(source : 'TSource, mapType : MapType) = (this :> IMapper<'TSource, 'TDestination>).Map(source, mapType)
--- Caelan.Frameworks.Common/Interfaces/IMapper.fs
+++ Caelan.Frameworks.Common/Interfaces/IMapper.fs
@@ -5,6 +5,4 @@ open Caelan.Frameworks.Common.Enums
 [<AllowNullLiteral>]
 type IMapper<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct> = 
     abstract Map : 'TSource -> 'TDestination
-    abstract Map : 'TSource * MapType -> 'TDestination
     abstract Map : 'TSource * 'TDestination byref -> unit
-    abstract Map : 'TSource * 'TDestination byref * MapType -> unit

603392b6d7f1abcd592cdb0e8fcad8e0488430fb
Matteo Bruni <ar3shack@gmail.com>
2015-02-05 10:24:40 +0100

    Delete IMapper.fs

--- Caelan.Frameworks.Common/IMapper.fs
+++
@@ -1,6 +0,0 @@
-﻿namespace Caelan.Frameworks.Common.Interfaces
-
-[<AllowNullLiteral>]
-type IMapper<'TSource, 'TDestination when 'TSource : equality and 'TSource : null and 'TSource : not struct and 'TDestination : equality and 'TDestination : null and 'TDestination : not struct> = 
-    abstract Map : 'TSource -> 'TDestination
-    abstract Map : 'TSource * 'TDestination byref -> unit

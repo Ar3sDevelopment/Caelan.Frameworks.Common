868340463deb8e7686ee611b83795c9d360f5fa2
Matteo Bruni <ar3shack@gmail.com>
2015-02-26 23:33:28 +0100

    Started converting NUnit F# project

---
+++ Caelan.Frameworks.Common.NUnit/BuilderTest.fs
@@ -0,0 +1,29 @@
+﻿namespace Caelan.Frameworks.Common.NUnit
+open System
+open System.Diagnostics
+open NUnit.Framework
+open Caelan.Frameworks.Common.Classes
+open Caelan.Frameworks.Common.Attributes
+
+[<MapEquals>]
+type TestA() =
+    member val A = "" with get, set
+
+    [<MapField("B")>]
+    member val C = "" with get, set
+
+[<MapEquals>]
+type TestB() =
+    member val A = "" with get, set
+
+    [<MapField("C")>]
+    member val B = "" with get, set
+
+[<TestFixture>]
+type BuilderTest() = 
+    [<Test>]
+    member __.TestNoBuilder() =
+        let stopwatch = Stopwatch()
+        stopwatch.Start()
+        stopwatch.Stop()
+        stopwatch.ElapsedMilliseconds |> printfn "%dms"
---
+++ Caelan.Frameworks.Common.NUnit/PasswordTest.fs
@@ -0,0 +1,17 @@
+﻿namespace Caelan.Frameworks.Common.NUnit
+open System
+open NUnit.Framework
+open Caelan.Frameworks.Common.Classes
+
+type CustomPasswordHasher() =
+    inherit PasswordHasher("salt", "default")
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member __.TestPassword() =
+        let pwd = CustomPasswordHasher()
+
+        "password" |> pwd.HashPassword |> printfn "%s"
+

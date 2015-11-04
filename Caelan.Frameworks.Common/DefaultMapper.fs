3a864950a3ddcd95706b3d358775d6deb4fba364
Matteo Bruni <ar3s@icloud.com>
2015-11-04 09:29:03 +0100

    refactoring mapper

--- Caelan.Frameworks.Common/DefaultMapper.fs
+++ Caelan.Frameworks.Common/DefaultMapper.fs
@@ -1,43 +1,40 @@
 ﻿namespace Caelan.Frameworks.Common.Classes
 open System
+open System.Reflection
 open Caelan.Frameworks.Common.Attributes
 open Caelan.Frameworks.Common.Interfaces
 
 type DefaultMapper<'TSource, 'TDestination>() = 
     interface IMapper<'TSource, 'TDestination> with
-        member this.Map(source) =
-            let destination = Activator.CreateInstance<'TDestination>()
-            (this :> IMapper<'TSource, 'TDestination>).Map(source, destination)
+        member this.Map(source) = (this :> IMapper<'TSource, 'TDestination>).Map(source, Activator.CreateInstance<'TDestination>())
         member this.Map(source, destination) =
-            let sourceType = source.GetType()
-            let customProperties = sourceType.GetProperties() |> Array.filter (fun t -> t.CustomAttributes |> Seq.length > 0)
-            match sourceType with
-            | _ when sourceType.GetCustomAttributes(typeof<MapEqualsAttribute>, true) |> Array.length > 0 ->
-                sourceType.GetProperties()
-                |> Array.choose (fun t -> match destination.GetType().GetProperty(t.Name) |> Option.ofObj with
-                                          | None -> None
-                                          | Some(property) -> Some (property, t.GetValue(source)))
-                |> Array.iter (fun (property, value) -> property.SetValue(destination, value))
-            | _ -> 
-                customProperties
-                |> Array.filter (fun t -> Attribute.GetCustomAttribute(t,typeof<MapEqualsAttribute>) |> (isNull >> not))
-                |> Array.iter (fun t ->
-                    match destination.GetType().GetProperty(t.Name) |> Option.ofObj with
-                    | None -> ()
-                    | Some(property) -> property.SetValue(destination, t.GetValue(source)))
-
-            customProperties
-            |> Array.map (fun t -> (t, Attribute.GetCustomAttribute(t,typeof<MapFieldAttribute>) :?> MapFieldAttribute))
-            |> Array.iter (fun (t, a) ->
-                match a |> Option.ofObj with
+            let iterGeneric name value =
+                match destination.GetType().GetProperty(name) |> Option.ofObj with
                 | None -> ()
-                | Some(a) ->
-                    match destination.GetType().GetProperty(a.ToField) |> Option.ofObj with
-                    | None -> ()
-                    | Some(property) -> property.SetValue(destination, t.GetValue(source)))
+                | Some(property) -> property.SetValue(destination, value)
+            let iterEqualsProperties (p : PropertyInfo) = (p.Name, p.GetValue(source)) ||> iterGeneric
+            let iterMapProperties (p : PropertyInfo, attribute : MapFieldAttribute) =
+                match attribute |> Option.ofObj with
+                | None -> ()
+                | Some(a) -> (a.ToField, p.GetValue(source)) ||> iterGeneric
             match source :> obj |> Option.ofObj with
             | None -> Unchecked.defaultof<'TDestination>
-            | Some(s) -> this.CustomMap(s :?> 'TSource, destination)
+            | Some(s) ->
+                let sourceType = source.GetType()
+                let customProperties = sourceType.GetProperties() |> Array.filter (fun t -> t.CustomAttributes |> Seq.length > 0)
+                match sourceType with
+                | st when st.GetCustomAttributes(typeof<MapEqualsAttribute>, true) |> Array.length > 0 ->
+                    st.GetProperties() |> Array.iter iterEqualsProperties
+                | _ -> 
+                    customProperties
+                    |> Array.filter (fun t -> Attribute.GetCustomAttribute(t,typeof<MapEqualsAttribute>) |> (isNull >> not))
+                    |> Array.iter iterEqualsProperties
+
+                customProperties
+                |> Array.map (fun t -> (t, Attribute.GetCustomAttribute(t,typeof<MapFieldAttribute>) :?> MapFieldAttribute))
+                |> Array.iter iterMapProperties
+                this.CustomMap(s :?> 'TSource, destination)
+                
 
     /// <summary>
     /// 

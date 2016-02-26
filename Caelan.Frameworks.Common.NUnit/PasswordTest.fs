5d3191f4d82613149628e21ee28efcd286e4bda7
Matteo Bruni <ar3s@icloud.com>
2016-02-26 12:23:43 +0100

    fix

--- Caelan.Frameworks.Common.NUnit/PasswordTest.fs
+++ Caelan.Frameworks.Common.NUnit/PasswordTest.fs
@@ -1,38 +1,19 @@
 ﻿namespace Caelan.Frameworks.Common.NUnit
+
 open System
 open NUnit.Framework
 open Caelan.Frameworks.Common.Classes
-open Caelan.Frameworks.PasswordEncryption.Classes
 open Caelan.Frameworks.PasswordHashing.Classes
 
-type CustomPasswordHasher() =
+type CustomPasswordHasher() = 
     inherit PasswordHasher("salt", "default")
-
-type CustomPasswordEncryptor() =
-    inherit PasswordEncryptor("default", "secret", "saltsalt")
 
 [<TestFixture>]
 type PasswordTest() = 
-
     [<Test>]
-    member __.TestHasher() =
+    member __.TestHasher() = 
         let pwd = CustomPasswordHasher()
-
-        "password" |> pwd.HashPassword |> printfn "%s"
+        "password"
+        |> pwd.HashPassword
+        |> printfn "%s"
         pwd.DefaultPasswordHashed |> printfn "%s"
-
-    [<Test>]
-    member __.TestEncryption() =
-        let pwd = CustomPasswordEncryptor()
-
-        let crypted = "password" |> pwd.EncryptPassword
-
-        crypted |> printfn "%s"
-
-        let decrypted = crypted |> pwd.DecryptPassword
-
-        (decrypted, "password") |> Assert.AreEqual
-
-        decrypted |> printfn "%s"
-
-        pwd.DefaultPasswordEncrypted |> printfn "%s"

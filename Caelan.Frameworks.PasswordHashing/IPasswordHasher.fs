145aa7b283e4482bdec35051709911c15f23bcb4
Matteo Bruni <ar3s@icloud.com>
2016-02-26 13:01:48 +0100

    removed hashing, changed tests

--- Caelan.Frameworks.PasswordHashing/IPasswordHasher.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace Caelan.Frameworks.PasswordHashing.Interfaces
-
-type IPasswordHasher = 
-    /// <summary>
-    /// This function hashes the given password
-    /// </summary>
-    /// <param name="password">The password to be hashed</param>
-    abstract HashPassword : password:string -> string

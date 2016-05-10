174112520e2ad7540a9152944ef4dd8ef172d546
Matteo Bruni <matteo.bruni@me.com>
2016-05-10 11:11:02 +0200

    conversion to c# completed

---
+++ Caelan.Frameworks.Common.NUnit/DateTimeRangeTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Caelan.Frameworks.Common.Classes;
+using NUnit.Framework;
+
+namespace Caelan.Frameworks.Common.NUnit
+{
+    [TestFixture]
+    public class DateTimeRangeTest
+    {
+        [Test]
+        public void TestRange()
+        {
+            var range = new DateTimeRange(DateTime.Today, DateTime.Now);
+
+            Assert.IsTrue(range.IsInRange(DateTime.Now.AddHours(-1)));
+        }
+    }
+}
